b1c6aefd57da5644552e1a1de084d4c94d336a81
Bryce 3D <94155427+Bryce-3D@users.noreply.github.com>
2022-01-01 13:48:02 +0800

    My code for 9A

---
+++ Python/0001-0100/CF_9A.py
@@ -0,0 +1,15 @@
+Rin = [ int(i) for i in input().split() ]
+Homura = max(Rin[0],Rin[1])
+
+if Homura == 1:
+    print('1/1')
+elif Homura == 2:
+    print('5/6')
+elif Homura == 3:
+    print('2/3')
+elif Homura == 4:
+    print('1/2')
+elif Homura == 5:
+    print('1/3')
+elif Homura == 6:
+    print('1/6')
