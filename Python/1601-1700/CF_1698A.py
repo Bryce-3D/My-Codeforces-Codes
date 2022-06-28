c4a4467210c474862e289ac5525e070c793aa2c7
Bryce 3D <94155427+Bryce-3D@users.noreply.github.com>
2022-06-29 01:39:52 +0800

    My code for 1698A in Python

---
+++ Python/1601-1700/CF_1698A.py
@@ -0,0 +1,9 @@
+#For fast I/O
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+for Homu in range(int(input())):
+    n = int(input())
+    a = [int(i) for i in input().split()]
+
+    print(a[0])
