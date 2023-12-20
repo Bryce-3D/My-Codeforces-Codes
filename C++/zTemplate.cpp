a0a91e156c1cc0f7bafecf4d8c1e2db5be0f5772
Bryce-3D <94155427+Bryce-3D@users.noreply.github.com>
2023-12-21 00:16:09 +0800

    Updated C++  Template

--- C++/zTemplate.cpp
+++ C++/zTemplate.cpp
@@ -1,34 +1,60 @@
 #include <bits/stdc++.h>
+
+// long long
 #define ll LoveLive
 #define LoveLive long long
+
+// Python Style
 #define True  true
 #define False false
 #define elif  else if
+
+// Make using pairs bearable
+#define p0 first
+#define p1 second
+
+// Useful "constants"
 #define MAX_INT 2147483647   // Max val for int
+#define FASTIO  ios_base::sync_with_stdio(False); cin.tie(0);
+
+// For loop macro
+#define FOR(x,a,b)   for (int x = (a); x < (b); x++)
+#define FORll(x,a,b) for (ll  x = (a); x < (b); x++)
 
 using namespace std;
+
+// Pair acronyms
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pllll;
+typedef pair<int,bool> pib;
+typedef pair<ll,bool> pllb;
 
 // Vector acronyms
 typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
 typedef vector<vector<vector<int>>> vvvi;
+
 typedef vector<bool> vb;
 typedef vector<vector<bool>> vvb;
 typedef vector<vector<vector<bool>>> vvvb;
+
 typedef vector<ll> vll;
 typedef vector<vector<ll>> vvll;
 typedef vector<vector<vector<ll>>> vvvll;
+
+typedef vector<pii> vpii;
 
 // Dict acronyms
 typedef unordered_map<int,int> dii;
 typedef unordered_map<int,bool> dib;
 typedef unordered_map<ll,ll> dllll;
 typedef unordered_map<ll,bool> dllb;
-typedef unordered_map<char,int> dci;
 
 // Set acronyms
 typedef set<int> si;
 typedef set<ll> sll;
+typedef set<vi> svi;
+typedef set<pii> spii;
 
 
 
