7beb6582740eb85b36975ab979a415285922cb7e
Bryce-3D <94155427+Bryce-3D@users.noreply.github.com>
2023-12-20 11:03:14 +0800

    Added Python and C++ Template Codes

---
+++ C++/zTemplate.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define ll LoveLive
+#define LoveLive long long
+#define True  true
+#define False false
+#define elif  else if
+#define MAX_INT 2147483647   // Max val for int
+
+using namespace std;
+
+// Vector acronyms
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<vector<bool>>> vvvb;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<vector<ll>>> vvvll;
+
+// Dict acronyms
+typedef unordered_map<int,int> dii;
+typedef unordered_map<int,bool> dib;
+typedef unordered_map<ll,ll> dllll;
+typedef unordered_map<ll,bool> dllb;
+typedef unordered_map<char,int> dci;
+
+// Set acronyms
+typedef set<int> si;
+typedef set<ll> sll;
+
+
+
+/* IDEA
+
+*/
+
+/* DEBUG LOG
+
+*/
+
+// Comment this out to turn off debugging
+// #define DEBUG
+
+
+void solve() {
+}
+
+
+int main() {
+    // Loop for Homu test cases
+    int Homu;
+    cin >> Homu;
+    for (int Kumi = 0; Kumi < Homu; Kumi++) {
+        solve();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// RECYCLING BIN --------------------------------------------------------------
---
+++ Python/zTemplate.py
@@ -0,0 +1,12 @@
+from collections import deque as dq
+
+#For fast I/O
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+'''IDEA
+
+'''
+
+for Homu in range(int(input())):
+    n = int(input())
